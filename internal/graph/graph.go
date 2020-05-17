0f7821774e34130d0f406e175091219aa52dca1a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-05-16 23:02:13 -0700

    internal/graph: weighted edges, shortest path, fix removal

--- internal/graph/graph.go
+++ internal/graph/graph.go
@@ -12,8 +12,8 @@ import (
 type Graph struct {
 	// adjacency represents graphs using an adjaency list. Vertices are
 	// represented using their hash codes for simpler equaliy checks.
-	adjacencyOut map[interface{}]map[interface{}]struct{}
-	adjacencyIn  map[interface{}]map[interface{}]struct{}
+	adjacencyOut map[interface{}]map[interface{}]int
+	adjacencyIn  map[interface{}]map[interface{}]int
 
 	// hash maintains the mapping of hash codes to the representative Vertex.
 	// It is assumed that two identical hashcodes of v1 and v2 are semantically
@@ -26,16 +26,57 @@ func (g *Graph) Add(v Vertex) Vertex {
 	g.init()
 	h := hashcode(v)
 	if _, ok := g.adjacencyOut[h]; !ok {
-		g.adjacencyOut[h] = make(map[interface{}]struct{})
-		g.adjacencyIn[h] = make(map[interface{}]struct{})
+		g.adjacencyOut[h] = make(map[interface{}]int)
+		g.adjacencyIn[h] = make(map[interface{}]int)
 		g.hash[h] = v
 	}
 	return v
 }
 
+// Remove removes the given vertex from the graph.
+func (g *Graph) Remove(v Vertex) Vertex {
+	// Note we don't need to call init here because delete() operations
+	// are all safe on nil maps.
+	h := hashcode(v)
+
+	// First, delete all our out-edges by deleting both the
+	// main key as well as any of the other nodes that are tracking the
+	// in edge
+	for out := range g.adjacencyOut[h] {
+		delete(g.adjacencyIn[out], h)
+	}
+	delete(g.adjacencyOut, h)
+
+	// Same as above but for in edges
+	for in := range g.adjacencyIn[h] {
+		delete(g.adjacencyOut[in], h)
+	}
+	delete(g.adjacencyIn, h)
+
+	// Forget this node completely
+	delete(g.hash, h)
+	return v
+}
+
+// Vertices returns the list of all the vertices in this graph.
+func (g *Graph) Vertices() []Vertex {
+	result := make([]Vertex, 0, len(g.hash))
+	for _, v := range g.hash {
+		result = append(result, v)
+	}
+
+	return result
+}
+
 // AddEdge adds a directed edge to the graph from v1 to v2. Both v1 and v2
 // must already be in the Graph via Add or this will do nothing.
 func (g *Graph) AddEdge(v1, v2 Vertex) {
+	g.AddEdgeWeighted(v1, v2, 0)
+}
+
+// AddEdgeWeighted adds a weighted edge. This is the same as AddEdge but
+// with the specified weight.
+func (g *Graph) AddEdgeWeighted(v1, v2 Vertex, weight int) {
 	g.init()
 	h1, h2 := hashcode(v1), hashcode(v2)
 
@@ -48,8 +89,8 @@ func (g *Graph) AddEdge(v1, v2 Vertex) {
 	inMap := g.adjacencyIn[h2]
 
 	// Add our edges
-	outMap[h2] = struct{}{}
-	inMap[h1] = struct{}{}
+	outMap[h2] = weight
+	inMap[h1] = weight
 }
 
 func (g *Graph) RemoveEdge(v1, v2 Vertex) {
@@ -105,14 +146,14 @@ func (g *Graph) Copy() *Graph {
 	g2.init()
 
 	for k, set := range g.adjacencyOut {
-		copy := make(map[interface{}]struct{})
+		copy := make(map[interface{}]int)
 		for k, v := range set {
 			copy[k] = v
 		}
 		g2.adjacencyOut[k] = copy
 	}
 	for k, set := range g.adjacencyIn {
-		copy := make(map[interface{}]struct{})
+		copy := make(map[interface{}]int)
 		for k, v := range set {
 			copy[k] = v
 		}
@@ -165,10 +206,10 @@ func (g *Graph) String() string {
 
 func (g *Graph) init() {
 	if g.adjacencyOut == nil {
-		g.adjacencyOut = make(map[interface{}]map[interface{}]struct{})
+		g.adjacencyOut = make(map[interface{}]map[interface{}]int)
 	}
 	if g.adjacencyIn == nil {
-		g.adjacencyIn = make(map[interface{}]map[interface{}]struct{})
+		g.adjacencyIn = make(map[interface{}]map[interface{}]int)
 	}
 	if g.hash == nil {
 		g.hash = make(map[interface{}]Vertex)
---
+++ internal/graph/path.go
@@ -0,0 +1,38 @@
+package graph
+
+// TopoShortestPath returns the shortest path information given the
+// topological sort of the graph L. L can be retrieved using any topological
+// sort algorithm such as KahnSort.
+//
+// The return value are two maps with the distance to and edge to information,
+// respectively. distTo maps the total distance from source to the given
+// vertex. edgeTo maps the previous edge to get to a vertex from source.
+func (g *Graph) TopoShortestPath(L []Vertex) (distTo map[interface{}]int, edgeTo map[interface{}]interface{}) {
+	/*
+	   Set the distance to the source to 0;
+	   Set the distances to all other vertices to infinity;
+	   For each vertex u in L
+	      - Walk through all neighbors v of u;
+	      - If dist(v) > dist(u) + w(u, v)
+	         - Set dist(v) <- dist(u) + w(u, v);
+	*/
+
+	distTo = map[interface{}]int{}
+	edgeTo = map[interface{}]interface{}{}
+
+	// For each vertex u in L
+	for _, u := range L {
+		// Walk through all neighbors v of u;
+		for vh, weight := range g.adjacencyOut[u] {
+			// x = dist(u) + w(u, v)
+			x := distTo[u] + weight
+
+			// If dist(v) > dist(u) + w(u, v)
+			if _, ok := distTo[vh]; !ok || distTo[vh] > x {
+				distTo[vh] = x
+			}
+		}
+	}
+
+	return distTo, edgeTo
+}
