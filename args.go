3b7bdfa6ef88195610ec133e8b72ed67735890fb
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-04-10 10:58:16 -0500

    Reword error message

--- args.go
+++ args.go
@@ -44,7 +44,7 @@ func newArgBuilder(opts ...Arg) (*argBuilder, error) {
 	var buildErr error
 	for _, opt := range opts {
 		if opt == nil {
-			return nil, fmt.Errorf("received nil arg")
+			return nil, fmt.Errorf("arg cannot be nil")
 		}
 		if err := opt(builder); err != nil {
 			buildErr = multierror.Append(buildErr, err)
